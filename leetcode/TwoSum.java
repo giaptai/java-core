96174025a69579b4288a426b037e23569d800187
kelving <gtainguyenk19@gmail.com>
2025-12-16 18:25:03 +0700

    fixed missing last element at Two sum excersice

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> containNum = new HashMap<>();
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
             if (containNum.containsKey(diff)) {
                 return new int[] { containNum.get(diff), i };
