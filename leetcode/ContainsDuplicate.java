6dd9df84fc3165f3f1d3798b72510f3974f910ed
kelving <gtainguyenk19@gmail.com>
2025-12-17 18:02:04 +0700

    added new solution at ContainsDuplicate exercise

--- leetcode/ContainsDuplicate.java
+++ leetcode/ContainsDuplicate.java
@@ -2,17 +2,29 @@ package leetcode;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
+    // solution 1 - HashMap
     public static boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> dupNum = new HashMap<>();
-        for (int item : nums) {
-            if (dupNum.containsKey(item)) {
+        Map<Integer, Integer> freqNum = new HashMap<>();
+        for (Integer i : nums) {
+            if (freqNum.containsKey(i)) {
                 return true;
             }
-            dupNum.put(item, 1);
+            freqNum.put(i, 1);
         }
         return false;
+    }
+
+    // solution 2 - Set
+    public static boolean containsDuplicate2(int[] nums) {
+        Set<Integer> freqNum = new HashSet<>();
+        for (Integer i : nums) {
+            freqNum.add(i);
+        }
+        return freqNum.size() != nums.length;
     }
 
     public static void main(String[] args) {
@@ -23,5 +35,9 @@ public class ContainsDuplicate {
         System.out.println(containsDuplicate(case1));
         System.out.println(containsDuplicate(case2));
         System.out.println(containsDuplicate(case3));
+
+        System.out.println(containsDuplicate2(case1));
+        System.out.println(containsDuplicate2(case2));
+        System.out.println(containsDuplicate2(case3));
     }
 }
