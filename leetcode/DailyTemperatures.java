1488a2208a92059790e504fb2f759e253e8ba2bd
kelving <gtainguyenk19@gmail.com>
2025-12-26 14:28:01 +0700

    finished solution 2 best optimized of Daily Temperatures excercise

--- leetcode/DailyTemperatures.java
+++ leetcode/DailyTemperatures.java
@@ -26,14 +26,24 @@ public class DailyTemperatures {
         return temperatures;
     }
 
-    // solution 2 - O(n)
+    // solution 2 - Space & Time O(n)
     public static int[] dailyTemperatures2(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-        return temperatures;
+        Stack<Integer> st = new Stack<>();
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
+                int idx = i - st.peek();
+                result[st.pop()] = idx;
+            }
+            st.push(i);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         int[] case1 = { 73, 74, 75, 71, 69, 72, 76, 73 };
-        System.out.println(Arrays.toString(dailyTemperatures(case1)));
+        int[] case2 = { 73, 74, 75, 71, 69 };
+        // System.out.println(Arrays.toString(dailyTemperatures(case1)));
+        System.out.println(Arrays.toString(dailyTemperatures2(case2)));
     }
 }
